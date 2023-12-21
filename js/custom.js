517ee6db32f63d9d280aee5bd5aa79b22c860961
sp-glitch <148739294+sp-glitch@users.noreply.github.com>
2023-12-22 08:01:02 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,45 +1,68 @@
-const MAIN_VISUAL_SLIDE = new Swiper(".main_visual_slide", {
+const MAIN_SLIDE = new Swiper(".main_slide", {
   loop: true,
-  parallax: true,
   speed: 1000,
+
+  autoplay: {
+    delay: 4500,
+    disableOnInteraction: false,
+  },
+
+  pagination: {
+    el: ".main_slide .swiper-pagination",
+    clickable: true,
+  },
 
   slideActiveClass: "on",
 });
 
-const MAIN_VS_LEFT_BUTTON = document.querySelector(
-  ".MainVisual .main_slide_arrows .left"
+const MAIN_SLIDE_LEFT_BUTTON = document.querySelector(
+  ".main_slide .arrows .left"
+);
+const MAIN_SLIDE_RIGHT_BUTTON = document.querySelector(
+  ".main_slide .arrows .right"
 );
 
-MAIN_VS_LEFT_BUTTON.addEventListener("click", () => {
-  MAIN_VISUAL_SLIDE.slidePrev();
+MAIN_SLIDE_LEFT_BUTTON.addEventListener("click", () => {
+  MAIN_SLIDE.slidePrev(1000);
+});
+MAIN_SLIDE_RIGHT_BUTTON.addEventListener("click", () => {
+  MAIN_SLIDE.slideNext(1000);
 });
 
-const MAIN_VS_RIGHT_BUTTON = document.querySelector(
-  ".MainVisual .main_slide_arrows .right"
-);
-
-MAIN_VS_RIGHT_BUTTON.addEventListener("click", () => {
-  MAIN_VISUAL_SLIDE.slideNext();
-});
-
-const MAIN_NOTICE_SLIDE = new Swiper(".main_notice_slide", {
+const MAIN_SUGGEST = new Swiper(".suggest_slide", {
   loop: true,
-  slidesPerView: 2.5,
+  slidesPerView: 3,
   spaceBetween: 30,
+  pagination: {
+    el: ".suggest_slide .swiper-pagination",
+    clickable: true,
+  },
 });
 
-const MAIN_MS_LEFT_BUTTON = document.querySelector(
-  ".MainCustomer .main_customer_arrows .left"
+const MAIN_SUGGEST_LEFT_BUTTON = document.querySelector(
+  ".MainSuggest .arrows .left"
+);
+const MAIN_SUGGEST_RIGHT_BUTTON = document.querySelector(
+  ".MainSuggest .arrows .right"
 );
 
-MAIN_MS_LEFT_BUTTON.addEventListener("click", () => {
-  MAIN_NOTICE_SLIDE.slidePrev();
+MAIN_SUGGEST_LEFT_BUTTON.addEventListener("click", () => {
+  MAIN_SUGGEST.slidePrev(1000);
+});
+MAIN_SUGGEST_RIGHT_BUTTON.addEventListener("click", () => {
+  MAIN_SUGGEST.slideNext(1000);
 });
 
-const MAIN_MS_RIGHT_BUTTON = document.querySelector(
-  ".MainCustomer .main_customer_arrows .right"
-);
+$(function(){
+  const tabBtn = $(".tab_btn>li");
+  const tabCont = $(".tab_cont>div");
+  tabCont.hide().eq(0).show();
 
-MAIN_MS_RIGHT_BUTTON.addEventListener("click", () => {
-  MAIN_NOTICE_SLIDE.slideNext();
+  tabBtn.on("click", function (e) {
+    e.preventDefault();
+    const index = $(this).index();
+
+    $(this).addClass("active").siblings().removeClass("active");
+    tabCont.eq(index).show().siblings().hide();
+  });
 });
